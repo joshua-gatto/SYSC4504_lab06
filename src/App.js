4edaccb223226fa6080d2105e4ff03e0a13f47f2
joshua-gatto <joshuagatto@cmail.carleton.ca>
2023-03-30 16:37:05 -0400

    changed program location

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
-
 
 function App() {
   return (
@@ -25,95 +23,5 @@ function App() {
   );
 }
 
-const Title = function(props) {
-  return <h1>{props.title}</h1>;
-}
-
-class Catalog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      filename: 'images/img1.jpg',
-      alt: 'image 1',
-      editing: false
-    };
-
-    this.renderNormal = this.renderNormal.bind(this);
-    this.renderEdit = this.renderEdit.bind(this);
-    this.editClick = this.editClick.bind(this);
-    this.saveClick = this.saveClick.bind(this);
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleAltChange = this.handleAltChange.bind(this);
-
-  }
-
-  renderNormal() {
-    return (
-      <div>
-        <h2>{this.state.alt}</h2>
-        <img src={this.state.filename} alt={this.state.alt} />
-        <button onClick={this.editClick}>Toggle Edit Mode</button>
-      </div>
-    );
-  }
-
-  renderEdit() {
-    return (
-      <div>
-        <p>
-          <label for="options">Filename: </label>
-          <select name="options" defaultValue={this.state.filename} onChange={this.handleNameChange}>
-            <option value="images/img1.jpg">Image 1</option>
-            <option value="images/img2.jpg">Image 2</option>
-            <option value="images/img3.jpg">Image 3</option>
-            <option value="images/img4.jpg">Image 4</option>
-          </select>
-        </p>
-        <p>
-          <label for="alt">Alt: </label>
-          <input name="alt" type="text" value={this.state.alt} onInput={this.handleAltChange}/>
-        </p>
-        <button onClick={this.saveClick}>Save</button>
-      </div>
-    );
-  }
-
-
-  handleAltChange(event) {
-    this.setState({alt: event.target.value});
-  }
-
-  handleNameChange(event) {
-    this.setState({filename: event.target.value});
-  }
-
-  editClick() {
-    this.setState({editing: true});
-  }
-
-  saveClick() {
-    this.setState({editing: false});
-  }
-
-  render() {
-    if (this.state.editing) {
-      return this.renderEdit();
-    } else {
-      return this.renderNormal();
-    }
-  }
-}
-
-function Lab06App() {
-  return (
-    <main>
-      <Title title = "Lab06 – React Application"/>
-      <Catalog Catalog />
-    </main>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById('react-lab'));
-root.render(<Lab06App />)
 
 export default App;
