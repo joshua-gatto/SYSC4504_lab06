69de1b96bf6d34ddd12eda7af4029083eb63d0d1
joshua-gatto <joshuagatto@cmail.carleton.ca>
2023-03-30 16:13:39 -0400

    Completed renderEdit() function in the Catalog class

--- src/App.js
+++ src/App.js
@@ -36,6 +36,12 @@ class Catalog extends React.Component {
       filename: 'images/img1.jpg',
       alt: 'image 1'
     };
+
+    this.renderNormal = this.renderNormal.bind(this);
+    this.renderEdit = this.renderEdit.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleAltChange = this.handleAltChange.bind(this);
+
   }
 
   renderNormal() {
@@ -51,7 +57,8 @@ class Catalog extends React.Component {
     return (
       <div>
         <p>
-          <select value={this.state.filename} onChange={this.handleNameChange}>
+          <label for="options">Filename: </label>
+          <select name="options" defaultValue={this.state.filename} onChange={this.handleNameChange}>
             <option value="images/img1.jpg">Image 1</option>
             <option value="images/img2.jpg">Image 2</option>
             <option value="images/img3.jpg">Image 3</option>
@@ -59,25 +66,33 @@ class Catalog extends React.Component {
           </select>
         </p>
         <p>
-          <input type="text" value={this.state.alt} onChange={this.handleAltChange} />
+          <label for="alt">Alt: </label>
+          <input name="alt" type="text" value={this.state.alt}/>
         </p>
-        <button onClick={this.saveClick}>Save</button>
+        <button onClick={this.handleAltChange}>Save</button>
       </div>
     );
   }
+
+
+  handleAltChange(event){
+    this.setState({alt: event.target.value});
+  }
+
+  handleNameChange(event) {
+    this.setState({filename: event.target.value});
+  }
   
   render() {
-    return this.renderNormal()
+    return this.renderEdit()
   }
 }
 
 function Lab06App() {
   return (
     <main>
-      <div>
       <Title title = "Lab06 – React Application"/>
       <Catalog Catalog />
-      </div>
     </main>
   );
 }
