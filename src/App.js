4810448f4599b07a8407e1f5e5dc7cd812567276
joshua-gatto <joshuagatto@cmail.carleton.ca>
2023-03-30 12:20:16 -0400

    added Lab06App() function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
@@ -21,5 +22,14 @@ function App() {
     </div>
   );
 }
+
+function Lab06App() {
+  return (
+    <main>
+      {/* add other components here */}
+    </main>
+  );
+}
+
 
 export default App;
